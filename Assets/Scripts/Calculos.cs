0e72d0e3b67b3fe89e2581c809862ec2a507980b
joaquinmora14 <joacomora13@gmail.com>
2025-03-28 10:43:10 -0300

    ejercicios 1.2, 1.3, 1.4

---
+++ Assets/Scripts/Calculos.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculos : MonoBehaviour
+{
+    public int numero1;
+    public int numero2;
+    public int resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = numero1 + numero2;
+        Debug.Log("El resultado es " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CalculosPersonalizados.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculosPersonalizados : MonoBehaviour
+{
+    public float numero1;
+    public float numero2;
+    float resultado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        resultado = numero1 + numero2;
+        Debug.Log("El resultado de la suma entre " + numero1 + " y " + numero2 + " es " + resultado);
+        resultado = numero1 - numero2;
+        Debug.Log("El resultado de la resta entre " + numero1 + " y " + numero2 + " es " + resultado);
+        resultado = numero1 * numero2;
+        Debug.Log("El resultado de la multiplicacion entre " + numero1 + " y " + numero2 + " es " + resultado);
+        resultado = numero1 / numero2;
+        Debug.Log("El resultado de la division entre " + numero1 + " y " + numero2 + " es " + resultado);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
